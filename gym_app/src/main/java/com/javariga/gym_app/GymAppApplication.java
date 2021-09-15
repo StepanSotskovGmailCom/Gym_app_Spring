8cd1f2f894a98a345a381f2ee3d2924780cb0ccb
StepansSotskovsGmailCom <stepan.sotskov@gmail.com>
2021-09-15 22:33:31 +0300

    Initial commit

---
+++ gym_app/src/main/java/com/javariga/gym_app/GymAppApplication.java
@@ -0,0 +1,13 @@
+package com.javariga.gym_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAppApplication.class, args);
+	}
+
+}
